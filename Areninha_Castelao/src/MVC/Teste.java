542af3e4e9846ef3085f5ec2340bc0faec8626ad
Ruinaldo <ruinaldo.ti@gmail.com>
2022-03-03 10:49:04 -0300

    Primeiro Commit

---
+++ Areninha_Castelao/src/MVC/Teste.java
@@ -0,0 +1,33 @@
+package MVC;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean num1 = false;
+		boolean num2 = true;
+		boolean num3 = true;
+		int num4 = 200;
+		
+		
+		//
+		if(num1 | num3) {
+			System.out.println("Entra na condição!");
+		}else {
+			System.out.println("Não entra");
+		}
+		
+		while (num4 > 30) {
+			num4 -= 1;
+		}
+		System.out.println(num4);
+		
+			
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Areninha_Castelao/src/module-info.java
@@ -0,0 +1,2 @@
+module Areninha_Castelao {
+}
